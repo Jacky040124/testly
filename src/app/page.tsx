000899948fad1d0d3462005c2c5e80397f17433f
Jacky Zhong <Jacky@dhcp-206-12-179-207.ubcsecure.wireless.ubc.ca>
2024-12-26 11:42:27 -0800

    feat(neondb) connected neon database to the codebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import { chat } from "@/service/chat.service";
 import { useState } from "react";
 import { ChatInterface } from "@/components/ChatInterface";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { getData } from "./action";
+import { UserData } from "@/components/UserData";
 
 export default function Home() {
   const [message, setMessage] = useState("");
   const [response, setResponse] = useState("");
+  const [data, setData] = useState("")
   const { user } = useUser();
 
   const handleSubmit = async () => {
@@ -15,11 +18,18 @@ export default function Home() {
     setResponse(result);
   };
 
+  const handleGetData = async () => {
+    setData(await getData());
+
+  }
+
   return (
     <div>
       <a href="/api/auth/login">Login</a>
       <a href="/api/auth/logout">Logout</a>
       <ChatInterface message={message} response={response} onMessageChange={setMessage} onSubmit={handleSubmit} />
+      <button onClick={handleGetData}>Get User Data</button>
+      {data && <UserData data={data} />}
     </div>
   );
 }
