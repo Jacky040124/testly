17cb52b99be58c0f36a7547ebae88be893ff8b66
Jacky <zhongzhenyu190@gmaio.com>
2024-12-26 12:10:03 -0800

    fix(minor): minor fix

--- src/app/action.ts
+++ src/app/action.ts
@@ -6,6 +6,6 @@ export async function getData() {
   const url = process.env.DATABASE_URL as string;
   const sql = neon(url);
 
-  const data = await sql`SELECT name, value FROM playing_with_neon WHERE id = 1`;
+  const data = await sql`SELECT name, value FROM playing_with_neon WHERE id = 2`;
   return JSON.stringify(data);
 }
