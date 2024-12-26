672d8d280c5fa7ea87c423a14f690245963c559d
Jacky Zhong <Jacky@MacBook-Air-2.local>
2024-12-25 19:47:37 -0800

    feat(chatbot): add chatgpt integration for the chatbot feature

---
+++ src/app/api/openai-chatbot/route.ts
@@ -0,0 +1,29 @@
+import OpenAI from "openai";
+import { NextResponse } from "next/server"
+
+const openai = new OpenAI({apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,});
+
+async function chat(content:string) {
+    const completion = openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    store: true,
+    messages: [{ role: "user", content: content }],
+    });
+
+    return (await completion).choices[0].message
+}
+
+
+export async function POST(request:Request) {
+    const { content } = await request.json();
+
+    try {
+        const result = await chat(content);
+        return NextResponse.json({ message: "Success", result: result.content }, { status: 200 });
+    } catch (error) {
+        console.error("OPENAI API error:", error);
+        return NextResponse.json({ error: "Failed to chat" }, { status: 400 });
+    }
+
+
+}
