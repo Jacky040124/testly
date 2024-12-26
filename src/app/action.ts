57f77aa11505ce79d5e94dd402bce0376f3aadc8
Jacky <zhongzhenyu190@gmail.com>
2024-12-26 11:42:27 -0800

    feat(neondb) connected neon database to the codebase

---
+++ src/app/action.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { neon } from "@neondatabase/serverless";
+
+export async function getData() {
+  const url = process.env.DATABASE_URL as string;
+  const sql = neon(url);
+
+  const data = await sql`SELECT name, value FROM playing_with_neon WHERE id = 1`;
+  return JSON.stringify(data);
+}
